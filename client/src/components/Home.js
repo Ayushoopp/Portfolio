130446020de1b7ef999eb866b9fb5007cad62300
Ayushoopp <you@example.com>
2023-03-07 14:36:15 +0530

    added backgroundColor50%

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import "./../App1.css"
 
 const Home = () => {
   return (
-    <div>
-      <p className='pt-5'>We are the Mern Developer from Home</p>
+    <>
+ 
+    <div className='Home' >
+      <p >We are the Mern Developer from Home</p>
       <h1>Ayush Mehrotra</h1>
     </div>
+
+    </>
   )
 }
 
