a3a000f4867cca0451f74f103fc9ab234fe97e27
Ayushoopp <you@example.com>
2023-03-08 11:52:58 +0530

    added login screen

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,15 +2,22 @@ import React from 'react'
 import "./../App.css"
 import "bootstrap/dist/css/bootstrap.css"
 import { NavLink } from 'react-router-dom'
+import loginlogo from "./../img/loginlogo.jpg"
 
 const Login = () => {
   return (
-    <div>
-     <form className="m-auto signupForm" action="">
-      <p>Email:</p>
-    <input type="email" name="email" placeholder='email@'  />
-    <p>Password :</p>
+    <div className="Box">
+     <img src={loginlogo} alt="" />
+     <form action="">
+      <h2>Email:</h2>
+        <input type="email" name="email" placeholder='email@'  />
+      <br />
+    <h2>Password :</h2>
     <input type="password" name="password" id="" placeholder='password' />
+    <br />
+   
+    <p></p>
+    <button type="submit" className="btn btn-primary"> Login</button>
      </form>
      <NavLink to="/signup" > Don't have an account</NavLink>
     </div>
