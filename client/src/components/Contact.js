a347de5d1697fd3492101bf8a2b9e344c4c7c683
Ayushoopp <you@example.com>
2023-03-13 23:47:32 +0530

    added contact page UI

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.css";
 
 const Contact = () => {
   return (
-   
-    <div >
-      <h1>Email Us For Any Queries</h1>
+   <>
+   <div className="d-flex flex-column">
+      <h1 >Email Us For Any Queries</h1>
       <p>Email: ayushmehrotra099@gmail.com</p>
 
       <h2>Social Media</h2>
     <a href='https://github.com/Ayushoopp'>github</a>
     </div>
+
+    {/* contact us form */}
+
+    <div class="mb-3 pt-5">
+    <h3>Get in Touch</h3>
+  <label for="exampleFormControlInput1" class="form-label">Email address:</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" required="true" placeholder="Your Email" />
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1"  class="form-label">Your Message</label>
+  <textarea class="form-control" requried="true" id="exampleFormControlTextarea1" cols="30" rows="3" placeholder='lorem ipsum'></textarea>
+</div>
+    <button type="submit" className="btn btn-primary">Submit</button>
+    </>
   )
 }
 
